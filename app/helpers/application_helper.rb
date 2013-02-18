8060f710062205023b19d8421090785173a0831a
root <niyi73@gmail.com>
2013-02-17 22:54:30 -0500

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+# Returns the full title on a per-page basis.				#documentation commment
+  def full_title(page_title)								#method definition
+    base_title = "Ruby on Rails Tutorial Sample App"		#variable assignment
+    if page_title.empty?									#condition statment
+      base_title											#implicit return
+    else														
+      "#{base_title} | #{page_title}"						#string interpolation
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 SampleApp::Application.routes.draw do
-  #get "static_pages/..."
+  get "users/new"
 
-  get "static_pages/home"
+  root to: 'static_pages#home'
 
-  get "static_pages/help"
+  match '/signup',  to: 'users#new'
 
-  get "static_pages/about"
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+  
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -63,4 +67,8 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+
+  # You can have the root of your site routed with "root"
+  # just remember to delete public/index.html.
+  # root :to => "welcome#index"
 end
